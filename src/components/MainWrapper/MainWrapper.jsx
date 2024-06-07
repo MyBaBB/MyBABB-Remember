b4a4a9fa7505cd6d5c0319a5e2bfffe53aadb2b8
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-07 09:50:13 -0700

    added bg image of Ocean at night underwater

--- src/components/MainWrapper/MainWrapper.jsx
+++ src/components/MainWrapper/MainWrapper.jsx
@@ -2,10 +2,11 @@
 import React from "react";
 import WholeTodo from "../../TodoFolder/WholeTodo.jsx";
 import Copyright from "../Copyright";
-
+import OceanNightImg from "./nightOcean.jpg";
 const MainWrapper = () => {
   return (
-    <div>
+    <div className="  w-screen  ">
+      <img src={OceanNightImg} alt="Ocean View At Night"  className="fixed width-cover"/>
       <div className=" relative m-auto flex  h-fit min-h-screen w-[100vw] p-2">
         <div className=" m-auto w-full p-4 ">
           <WholeTodo />
