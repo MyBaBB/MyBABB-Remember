955c15d22cb9b31ca7ae485749a036683e87cbad
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-08 08:19:38 -0700

    test route

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./" , 
-	  plugins: [react(),
-	    VitePWA({
-	  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 
-	 'mask-icon.svg'],
-	      manifest: {
-	        name: 'My Awesome App',
-	        short_name: 'MyApp',
-	        description: 'My Awesome App description',
-	        theme_color: '#ffffff',
-	        icons: [
-	          {
-	            src: 'pwa-192x192.png',             },
-	          {
-	            src: 'pwa-512x512.png',
-	            sizes: '512x512',
-	            type: 'image/png'
-	          }
-        ]
-        }
-      }),
-    ],
+  base: "./ https://mybabb.github.io/MyBABB-Remember/" , 
+  plugins: [react()],
 })
