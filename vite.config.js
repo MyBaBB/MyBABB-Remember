f5837ce686c847ad91e05457aacac4794e3bee98
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-08 10:30:46 -0700

    added purpose: any maskable, to the icons removed the 612 image ref was error

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         
         icons: [
-          {
-            src: './pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: "any maskable",
-      },
+          
         {
             src: './pwa-512x512.png ',
              sizes: '512x512',
