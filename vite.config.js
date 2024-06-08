f87d66592bc7994d22430104a7e639afaaf260c8
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-08 08:20:46 -0700

    test route

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./ https://mybabb.github.io/MyBABB-Remember/" , 
+  base: "./mybabb.github.io/MyBABB-Remember/" , 
   plugins: [react()],
 })
