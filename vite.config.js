0da73a6528ac053c0e30c3126d86bcf28e71aa9f
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-08 10:17:41 -0700

    image errors it works in dev mode but not live , testing to figure it out.

--- vite.config.js
+++ vite.config.js
@@ -25,12 +25,13 @@ export default defineConfig({
           //   sizes: '192x192',
           //   type: 'image/png'
           // },
-          // {
-          //   src: './pwa-512x512.png',
-          //   sizes: '512x512',
-          //   type: 'image/png'
-          // }
-        ]
+        {
+            src: './pwa-64x64.png',
+             sizes: '64x64',
+            type: 'image/png',
+            purpose: "any maskable",
+         }
+        ]  
       }
     })
   ],
