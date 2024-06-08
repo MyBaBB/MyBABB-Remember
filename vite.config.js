5ff57800d3816ab2eccfe3488089ff2c5e3a61cd
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-08 07:06:40 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "./" , 
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+        manifest: {
+          name: 'My Awesome App',
+	        short_name: 'MyApp',
+	        description: 'My Awesome App description',
+	        theme_color: '#ffffff',
+
+          Icons: [
+            {
+              src: "./src/assets/WooduckFavicon.ico",
+              sizes: "80x80",
+              type: "image/png",
+              purpose: "any maskable",
+             
+            },
+          
+          ],
+        }
+      }),
+    ],
 })
+ 
+
+  
