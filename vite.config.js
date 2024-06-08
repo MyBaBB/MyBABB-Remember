76c4fef59635e6aea5c409c36efa6f0211a51692
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-08 10:09:31 -0700

    image errors it works in dev mode but not live , testing to figure it out.

--- vite.config.js
+++ vite.config.js
@@ -20,11 +20,11 @@ export default defineConfig({
         theme_color: '#ffffff',
         
         icons: [
-          {
-            src: './Readme.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
+          // {
+          //   src: './Readme.png',
+          //   sizes: '192x192',
+          //   type: 'image/png'
+          // },
           // {
           //   src: './pwa-512x512.png',
           //   sizes: '512x512',
