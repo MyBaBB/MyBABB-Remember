f4e902a55eaea8ecdf9edc9d215fb372f9cf4b43
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-12 05:00:22 -0700

    cleaner

--- src/TodoFolder/TodoList.jsx
+++ src/TodoFolder/TodoList.jsx
@@ -3,36 +3,37 @@ import { TodoItem } from "./TodoItem";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
-//     <ul className="list -mt-4 mb-2 text-center font-Itim-Regular">
-//       {todos.length === 0 && "Your Notes Appear Here"}
-//       {todos.map((todo) => {
-//         return (
-//           <TodoItem
-//             {...todo}
-//             key={todo.id}
-//             toggleTodo={toggleTodo}
-//             deleteTodo={deleteTodo}
-//           />
-//         );
-//       })}
-//     </ul>
-//   );
-// }
-
-
-
-  <ul className="list -mt-4 mb-2 text-center font-Itim-Regular">
-{todos.slice().reverse().map((todo) => {
-  return (
-    <TodoItem
-      {...todo}
-      key={todo.id}
-      toggleTodo={toggleTodo}
-      deleteTodo={deleteTodo}
-    />
-  );
-})}
-</ul> 
+    <ul className="list -mt-4 mb-2 text-center font-Itim-Regular">
+      {todos.length === 0 && "Your Notes Appear Here"}
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </ul>
   );
 }
+
+
+
+//   <ul className="list -mt-4 mb-2 text-center font-Itim-Regular">
+// {todos.length === 0 && "Your Notes Appear Here"}
+// {todos.slice().reverse().map((todo) => {
+//   return (
+//     <TodoItem
+//       {...todo}
+//       key={todo.id}
+//       toggleTodo={toggleTodo}
+//       deleteTodo={deleteTodo}
+//     />
+//   );
+// })}
+// </ul> 
+//   );
+// }
      
