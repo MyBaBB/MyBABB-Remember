aad7852183daf20a138a7e625980362fb1bf2fe2
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-27 07:30:19 -0700

    no good uninstall button

--- src/components/MainWrapper/PWAFolder/WebApp.jsx
+++ src/components/MainWrapper/PWAFolder/WebApp.jsx
@@ -2,15 +2,7 @@
 import React from "react";
 import "./WebApp.css";
 
-const handleUninstall = () => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-      for (const registration of registrations) {
-        registration.unregister();
-      }
-    });
-  }
-};
+ 
 const InstallApp = () => {
   let deferredPrompt;
   window.addEventListener("beforeinstallprompt", (e) => {
