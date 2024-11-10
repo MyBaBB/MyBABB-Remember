9584f11ae63fdc679d9d561691ca029fa99fdcf1
Brett Baker <GitBaBB@MyBaBB.com>
2024-11-10 11:37:52 -0800

    try router swith on the app

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <BrowserRouter>
-        <Switch>
-           <Route path="/" element={<App />} />
-        </Switch>
-      </BrowserRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
