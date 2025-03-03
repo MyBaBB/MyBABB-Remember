bdcc3ba4b06130e0f60e0fb6ec713f059bd04a44
Brett Baker <GitBaBB@MyBaBB.com>
2025-03-03 05:25:03 -0800

    made a screen saver adjuster for time control on large screens.

--- src/components/ScreenSaver.jsx
+++ src/components/ScreenSaver.jsx
@@ -1,11 +1,10 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-
-
+import React, { useState, useEffect } from 'react';
+import './ScreenSaver.css';
+import { FaCaretDown } from "react-icons/fa6";
 
 let timerID = null;
-let timeout = 300000;
-
+let timeout = 300000; // initial timeout value (5 minutes)
 
 function resetTimer() {
   if (timerID) {
@@ -15,31 +14,75 @@ function resetTimer() {
   if (window.innerWidth < 640) {
     return;
   } else {
-    // Set timeout to 300000 milliseconds (5 minutes)
-    timeout = 300000;
+    timerID = setTimeout(() => {
+      window.location.href = 'https://screen.saver.mybabb.com/fishynotepad'
+    }, timeout);
   }
-  timerID = setTimeout(() => {
-    window.location.href = 'https://screen.saver.mybabb.com/fishynotepad'
-  }, timeout);
 }
 
-window.addEventListener('mousemove', resetTimer);
-window.addEventListener('touchstart', resetTimer);
+function ScreenSaver() {
+  const [isScreensaverOn, setIsScreensaverOn] = useState(true);
+  const [timeoutValue, setTimeoutValue] = useState(300000); // new state for timeout value
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
+  useEffect(() => {
+    if (isScreensaverOn) {
+      window.addEventListener('mousemove', resetTimer);
+      window.addEventListener('touchstart', resetTimer);
+    } else {
+      window.removeEventListener('mousemove', resetTimer);
+      window.removeEventListener('touchstart', resetTimer);
+    }
+  }, [isScreensaverOn]);
 
+  const handleToggleScreensaver = () => {
+    setIsScreensaverOn(!isScreensaverOn);
+  };
 
+  const handleTimeoutChange = (event) => {
+    const newTimeoutValue = parseInt(event.target.value);
+    setTimeoutValue(newTimeoutValue);
+    timeout = newTimeoutValue; // update the timeout variable
+  };
 
+  const handleAccordionToggle = () => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
 
-
-
-const ScreenSaver = () => {
   return (
-    <div>
-        {resetTimer()}
+    <>
+    <div className='fixed top-10 right-10 hidden lg:block'>
+    <div className="accordion" style={{ width: '8rem' }}>
+      <div className="accordion-header  " onClick={handleAccordionToggle}>
+        <span>Screensaver</span> 
        
-
+        <span className={`accordion-icon ${isAccordionOpen ? 'open' : ''}`}>
+        <FaCaretDown />
+        </span>
+      </div>
+      {isAccordionOpen && (
+        <div className="accordion-content">
+          <button onClick={handleToggleScreensaver}>
+            {isScreensaverOn ? 'Turn off' : 'Turn on '}
+          </button>
+          <div className="slider-container">
+            <input
+              type="range"
+              min="3000" // 30 seconds
+              max="900000" // 15 minutes
+              step="30000" // 30 seconds
+              value={timeoutValue}
+              onChange={handleTimeoutChange}
+              orient="vertical"
+            />
+            <span>{timeoutValue / 60000}&nbsp;minutes</span>
+          </div>
+        </div>
+      )}
     </div>
-  )
+    </div>
+    </>
+  );
 }
 
-export default ScreenSaver
+export default ScreenSaver;
